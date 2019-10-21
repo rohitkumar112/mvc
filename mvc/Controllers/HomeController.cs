cd4f636d5a92e20f33a25e0140ad269fcea5c969
Rohit <k152273@nu.edu.pk>
2019-10-21 21:44:36 +0500

    Add project files.

---
+++ mvc/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc.Models;
+namespace mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+        public ActionResult rohit()
+        {
+          //  var data = getemplyoeedetails();
+            return View();
+        }
+        /*
+        private model1 getemplyoeedetails()
+        {
+            return new model1()
+            {
+                id = 1,
+                name = "rohit",
+                address = "qureshi mohola",
+            }; 
+               
+        }
+        */
+        
+        
+        
+    }
+}
---
+++ mvc/Models/model1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace mvc.Models
+{
+    public class model1
+    {
+            public int id { set; get; }
+          public String name { get; set; }
+          public String address { get; set; }
+        public int getname { set; get; }
+
+    }
+}
